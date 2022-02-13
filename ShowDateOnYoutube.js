6bb20a9198f9fdff6dc40475a4684cd59a58b00c
George Bobrov <skivvvvy@gmail.com>
2022-02-13 16:02:21 +0200

    move download and create clip buttons to popup menu

--- ShowDateOnYoutube.js
+++ ShowDateOnYoutube.js
@@ -4,7 +4,7 @@
 console.log('ShowDateOnYoutube.js');
 
 document.addEventListener("yt-navigate-finish", function(event) {
-	// console.log("yt-navigate-finish from ShowDateOnYoutube")
+    // console.log("yt-navigate-finish from ShowDateOnYoutube")
 
     if (location.pathname == "/watch") 
         setTimeout(() => {
@@ -14,13 +14,54 @@ document.addEventListener("yt-navigate-finish", function(event) {
                 info.style.display = "unset"
             }
 
+
+            // Hide Download button
             let downloadButton = document.querySelector('#top-level-buttons-computed > ytd-download-button-renderer')
-            if (downloadButton) downloadButton.style.display = "none"
-            
+            if (downloadButton) {
+                console.log("Hide Download button", downloadButton.querySelector("#text").textContent)
+
+                downloadButton.style.display = "none"
+            }
+ 
+            // Hide MakeClip button
             let buttons = document.querySelectorAll("#top-level-buttons-computed > ytd-button-renderer")
             let clipButton = [...buttons].filter(el => el.querySelector("#text").textContent == 'Создать клип')[0]
-            if (clipButton) clipButton.style.display = "none"
+            if (clipButton) {
+                console.log("Hide MakeClip button", clipButton.querySelector("#text").textContent)
+                clipButton.style.display = "none"
+            }
 
+            // Show Save button if hidden
+            let saveButton = [...buttons].filter(el => el.querySelector("#text").textContent == 'Сохранить')[0]
+            if (saveButton) {
+                console.log("Show Save button if hidden, was", saveButton.style.display, "visible")
+                saveButton.style.display = ""
+            }
+
+            // Remove all buttons from PopupMemu (those left over from previous video)
+            let buttonsInPopup = document.querySelectorAll('ytd-menu-popup-renderer > ytd-download-button-renderer, ytd-menu-popup-renderer > ytd-button-renderer');
+            for (var buttonInPopup of buttonsInPopup) 
+                buttonInPopup.remove()
+
+            // Move Save and MakeClip buttons to PopupMemu when PopupMemu opens (initially it may not exist)
+            document.querySelector('yt-icon-button.dropdown-trigger').addEventListener("click", onOpenPopupMemu, { once: true })
+            
+            function onOpenPopupMemu(event) {
+                console.log("dropdown-trigger")
+
+                let popupMenu = document.querySelector("ytd-menu-popup-renderer")
+
+                if (downloadButton) {
+                    downloadButton.style.display = ""
+                    popupMenu.append(downloadButton)
+                }
+
+                if (clipButton) {
+                    clipButton.style.display = ""
+                    popupMenu.append(clipButton)
+                }
+
+            }
         }, 1000);
 })
 
