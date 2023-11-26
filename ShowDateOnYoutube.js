4ea06d97806c8326c24321cfc7af1d02526a3aef
George Bobrov <skivvvvy@gmail.com>
2023-11-26 20:35:56 +0200

    Get date from meta works wrong. Back to old method

--- ShowDateOnYoutube.js
+++ ShowDateOnYoutube.js
@@ -1,5 +1,5 @@
 // Youtube hides the publication date of the video if the description of the video is not open. 
-// This code allows you to take the publication date in HTML metainformation and show it.
+// This code allows you to take the date of publication of the video in the tooltip and show it without opening the description.
 console.log('ShowDateOnYoutube.js');
 
 document.addEventListener("yt-navigate-finish", function(event) {
@@ -7,25 +7,42 @@ document.addEventListener("yt-navigate-finish", function(event) {
 
     if (location.pathname == "/watch") 
         setTimeout(() => {
-// <meta itemprop="datePublished" content="2020-03-23T12:15:00-07:00"> 
-            var datePublishedStr = document.querySelector("meta[itemprop='datePublished']")?.content
+            var dateInfoTooltip = document.querySelector("#description #tooltip")
+            if (dateInfoTooltip) {
+                var str = dateInfoTooltip.textContent
+                // str = '\n  65 127 просмотров • Прямой эфир состоялся 30 окт. 2023 г. • #TheGideonGames #AlanWake2Прохождение #AlanWake2\n'
 
-            if (datePublishedStr) {
-                var datePublished = new Date(datePublishedStr)
+                // str.split('•')
 
-                var options = {
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric',
-                };
+                // отсечь количество просмотров
+                var ind = str.indexOf('•')
+                if (ind > -1)
+                    str = str.slice(ind + 1)
 
-                var date = datePublished.toLocaleDateString(undefined, options) 
-                var dateInfo = document.querySelector("#description #info > span:nth-child(3)")
-                if (dateInfo) 
+                // отсечь теги
+                var ind = str.indexOf('•')
+                if (ind > -1)
+                    str = str.slice(0, ind)    
+
+                // найти первую цифру - начало даты
+                let matchList = str.match(/\d/);
+                if (matchList)
+                    str = str.slice(matchList.index)
+
+                // удалить перевод строки
+                var date = str.replace('\n', '')
+                if (date.length < 20) {
+                    var dateInfo = document.querySelector("#description #info > span:nth-child(3)")
                     dateInfo.textContent = ' •  ' + date + '  •  ' + dateInfo.textContent + '  •'; 
+                }
             }
 
         }, 1000);
 })
+
+
+// var dateInfoMeta = document.querySelector("meta[itemprop='datePublished']").content
+// Получение даты публикации из метаинформация работает неверно при переходе на другое видео. Метаинформация страницы при этом не меняется, осаётся от старого видео. 
+
 
 
